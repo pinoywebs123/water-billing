47409a15f382a2d460b14f377f743d7f55846d21
morley gwapo <moravel752@gmail.com>
2019-08-06 15:17:31 +0800

    adding basic TDD testcase for login

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,9 +4,11 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -14,8 +16,63 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/auth/login');
 
         $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
     }
+
+    public function testInvalidEmailPassword()
+    {
+        $response = $this->post('/auth/login', [
+            'email'     => 'NOT_FOUND',
+            'password'  => 'NOT_FOUND'
+        ]);
+
+        $response->assertStatus(302);
+        $this->assertGuest();
+    }
+
+    public function testLoginAsAdmin()
+    {
+        $user = User::create([
+            'name'      => 'morley',
+            'email'     => 'tae@yahoo.com',
+            'password'  => bcrypt('password'),
+            'role_id'   => 1
+        ]);
+
+        $response = $this->post('/auth/login', [
+            'email'     => $user->email,
+            'password'  => 'password'
+        ]);
+
+
+        $response->assertRedirect('/admin/home');
+        $this->actingAs($user);
+
+
+    }
+
+    public function testLoginAsBilling()
+    {
+        $user = User::create([
+            'name'      => 'morley',
+            'email'     => 'tae@yahoo.com',
+            'password'  => bcrypt('password'),
+            'role_id'   => 2
+        ]);
+
+        $response = $this->post('/auth/login', [
+            'email'     => $user->email,
+            'password'  => 'password'
+        ]);
+
+
+       
+        $user = $this->actingAs($user);
+        $response->assertRedirect('/billing/home');
+    }
+
+    
 }
