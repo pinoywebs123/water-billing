ddb62c974e42a3716869dc0bc62acca9b719014e
llnt0821 <38583661+codingsfun@users.noreply.github.com>
2019-09-23 14:17:40 +0800

    Change client registration ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
   
   .well{
     border-radius: 30px;
-    margin-top: 10%;
+    margin-top: 10px;
     opacity: 0.9;
   }
   .input-group{
@@ -50,7 +50,7 @@ p, label {
 
 <div class="container">
     
-    <div class="col-md-6 col-md-offset-3 well">
+    <div class="col-md-8 col-md-offset-2 well">
       <center>
        <img src="{{URL::to('images/logo2.png')}}" width="120px">
        <br /><br />
@@ -64,56 +64,92 @@ p, label {
       @include('shared.notif')
 
         <form action="{{ route('store_profile_user') }}" method="post">
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">E-mail:</label>
-                <input type="email" class="form-control form-control-sm" id="email" name="email" required placeholder="my_name@email.com">
-            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">E-mail:</label>
+                            <input type="email" class="form-control form-control-sm" id="email" name="email" required placeholder="my_name@email.com">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Gender</label>
+                            <select class="form-control form-control-sm" id="gender" name="gender" required>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm" class="col-form-label col-form-label-sm">First Name:</label>
-                <input type="text" class="form-control form-control-sm" id="first_name" name="first_name" required placeholder="Enter first name">
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Middle Name:</label>
-                <input type="text" class="form-control form-control-sm" id="middle_name" name="middle_name" required placeholder="Enter middle name">
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Last Name:</label>
-                <input type="text" class="form-control form-control-sm" id="last_name" name="last_name" required placeholder="Enter last name">
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Birth Date:</label>
-                <input type="date" class="form-control form-control-sm" id="birth_date" name="birth_date" required>
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Gender</label>
-                <select class="form-control form-control-sm" id="gender" name="gender" required>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Contact number</label>
-                <input type="tel" class="form-control form-control-sm" id="contact" name="contact" required pattern="[0-9]{11}" placeholder="09123456789"">
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Address:</label>
-                <input type="text" class="form-control form-control-sm" id="address" name="address" required placeholder="123 Street">
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">City:</label>
-                <input type="text" class="form-control form-control-sm" id="city" name="city" required placeholder="Cebu City">
-            </div>
-            <div class="form-group">
-                <label for="" class="col-form-label col-form-label-sm">Province:</label>
-                <input type="text" class="form-control form-control-sm" id="province" name="province" required placeholder="Cebu">
-            </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm" class="col-form-label col-form-label-sm">First Name:</label>
+                            <input type="text" class="form-control form-control-sm" id="first_name" name="first_name" required placeholder="Enter first name">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Contact number</label>
+                            <input type="tel" class="form-control form-control-sm" id="contact" name="contact" required pattern="[0-9]{11}" placeholder="09123456789"">
+                        </div>
+                    </div>
+                </div>
 
-            <div class="text-center"> 
-                <button type="submit" class="btn btn-primary">Submit</button> 
-                <a href="{{ route('login') }}" class="btn btn-default">Done</a> 
-            </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Middle Name:</label>
+                            <input type="text" class="form-control form-control-sm" id="middle_name" name="middle_name" required placeholder="Enter middle name">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Address:</label>
+                            <input type="text" class="form-control form-control-sm" id="address" name="address" required placeholder="123 Street">
+                        </div>
+                    </div>
+                </div>
 
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Last Name:</label>
+                            <input type="text" class="form-control form-control-sm" id="last_name" name="last_name" required placeholder="Enter last name">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">City:</label>
+                            <input type="text" class="form-control form-control-sm" id="city" name="city" required placeholder="Cebu City">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Birth Date:</label>
+                            <input type="date" class="form-control form-control-sm" id="birth_date" name="birth_date" required>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="" class="col-form-label col-form-label-sm">Province:</label>
+                            <input type="text" class="form-control form-control-sm" id="province" name="province" required placeholder="Cebu">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col text-center">
+                        <button type="submit" class="btn btn-primary">Submit</button> 
+                        <a href="{{ route('login') }}" class="btn btn-default">Done</a> 
+                    </div>
+                </div>
+            </div>
             @csrf
         </form>
 
