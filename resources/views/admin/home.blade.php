7f219d20cea52af602e482156318485f3d2a7d42
llnt0821 <38583661+codingsfun@users.noreply.github.com>
2019-12-12 16:19:34 +0800

    Income and Water Consumption chart now functioning.

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,20 +5,46 @@
 @endsection
 
 @section('contents')
-    <div class="row">
-        <div class="col-md-6">
-            <canvas id="myChart"></canvas>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <canvas id="myChart"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="myChart2"></canvas>
+            </div>
         </div>
-        <div class="col-md-6">
-            <canvas id="myChart2"></canvas>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <button id="try" class="btn btn-primary">Try</button>
-        </div>
-        <div class="col-md-6">
-            <button id="try2" class="btn btn-primary">Try</button>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <input type="date" id="filter_income_from" class="form-control">
+                        </div>
+                        <div class="col-md-4">
+                                <input type="date" id="filter_income_to" class="form-control">
+                        </div>
+                        <div class="col-md-4">
+                            <button id="filter_income" class="btn btn-primary">Filter income</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <input type="date" id="filter_cons_from" class="form-control">
+                        </div>
+                        <div class="col-md-4">
+                                <input type="date" id="filter_cons_to" class="form-control">
+                        </div>
+                        <div class="col-md-4">
+                            <button id="filter_consumption" class="btn btn-primary">Filter consumption</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 	
@@ -36,15 +62,95 @@
         secondChart();
     });
 
-    $('#try').click(function() {
-        firstChart();
+    var months = '';
+    function getMonths() {
+        
+    }
+
+    $('#filter_income').click(function() {
+        var from = $('#filter_income_from').val();
+        var to = $('#filter_income_to').val();
+
+        $.ajax({
+            method: 'GET', // Type of response and matches what we said in the route
+            url: '{{ route("filter_income_chart")}}', // This is the url we gave in the route
+            data: {'from' : from, 'to' : to}, // a JSON object to send back
+            success: function(response){ // What to do if we succeed
+                console.log(response);
+                var chart = new Chart(ctx, {
+                    // The type of chart we want to create
+                    type: 'line',
+
+                    // The data for our dataset
+                    data: {
+                        labels: //['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                            response[0]
+                        ,
+                        datasets: [{
+                            label: 'Income Chart',
+                            
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: //[0, 10]
+                                response[1]
+                        }]
+                    },
+
+                    // Configuration options go here
+                    options: {}
+                });
+
+            },
+            error: function(jqXHR, textStatus, errorThrown) { // What to do if we fail
+                console.log(JSON.stringify(jqXHR));
+                console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
+            }
+        });
+        
     });
 
-    $('#try2').click(function() {
-        secondChart();
+    $('#filter_consumption').click(function() {
+        var from = $('#filter_cons_from').val();
+        var to = $('#filter_cons_to').val();
+
+        $.ajax({
+            method: 'GET', // Type of response and matches what we said in the route
+            url: '{{ route("filter_consumption_chart")}}', // This is the url we gave in the route
+            data: {'from' : from, 'to' : to}, // a JSON object to send back
+            success: function(response){ // What to do if we succeed
+                console.log(response);
+                var chart = new Chart(ctx2, {
+                    // The type of chart we want to create
+                    type: 'line',
+
+                    // The data for our dataset
+                    data: {
+                        labels: //['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                            response[0]
+                        ,
+                        datasets: [{
+                            label: 'Water Consumptions Chart',
+                            
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: //[0, 10]
+                                response[1]
+                        }]
+                    },
+
+                    // Configuration options go here
+                    options: {}
+                });
+
+            },
+            error: function(jqXHR, textStatus, errorThrown) { // What to do if we fail
+                console.log(JSON.stringify(jqXHR));
+                console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
+            }
+        });
+        
     });
 
-    function firstChart() {
+    function firstChart(month, income) {
+
         var chart = new Chart(ctx, {
             // The type of chart we want to create
             type: 'line',
@@ -99,6 +205,7 @@
             // Configuration options go here
             options: {}
         });
+
     }
 
     function secondChart() {
@@ -130,7 +237,7 @@
                     ?>
                 ],
                 datasets: [{
-                    label: 'Water Consumptions',
+                    label: 'Water Consumptions Chart',
                     
                     borderColor: 'rgb(255, 99, 132)',
                     data: //[0, 10, 5, 2, 20, 22, 10]
