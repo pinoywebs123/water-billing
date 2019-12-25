b7d5b811ac7b00f7baeb59f5f4d497a3b5b66c0e
morls abu <moravel72@gmail.com>
2019-12-25 21:42:06 +0800

    billing paid status 5

--- app/Http/Controllers/Billing/UserController.php
+++ app/Http/Controllers/Billing/UserController.php
@@ -15,8 +15,8 @@ class UserController extends Controller
     
     public function home()
     {
-        $total = Billing::where('status_id',1)->sum('bill');
-        $unpaid = Billing::where('status_id',1)->get();
+        $total = Billing::where('status_id',5)->sum('bill');
+        $unpaid = Billing::where('status_id',5)->get();
         $consumption = DB::select('SELECT MONTH(end_date) as month, sum(water_consumption) as monthly_ws
         FROM billings GROUP BY MONTH(end_date) ORDER BY MONTH(end_date)');
 
